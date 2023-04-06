d28b4b01df2a132eb453aa124931b32986079603
unknown <cauaoliveirabarbosa7@gmail.com>
2023-04-06 16:29:49 -0300

    Este codigo serve para dizer os numeros pares e impares de uma sequencia

--- exercicio4.py
+++ exercicio4.py
@@ -1,1 +1,8 @@
-lista = ('a', 'e', 'i', 'o', 'u', 'c', 'd', 'e', 'f', 'g',)
+palavra = ['c', 'a', 'u', 'ã', 'b', 'a', 'r', 'b', 'o', 's',]
+v = ['a', 'e', 'i', 'o', 'u',]
+palavrasqtd= len(palavra)
+quantidadev= len(v)
+quantidadec= palavrasqtd - quantidadev
+result =str(quantidadec)
+if palavra not in v:
+    print ("A quantidade de Consoantes é:"  + result)
