83885ed7c7133b2372492c25ccf42abb704e9842
unknown <cauaoliveirabarbosa7@gmail.com>
2023-04-13 11:34:50 -0300

    O vetor media serve para armazenar as notas dos aluno

---
+++ exercicio6.py
@@ -0,0 +1,24 @@
+# criar um vetor para armazenar as médias dos alunos
+medias = []
+
+# iterar sobre cada aluno
+for i in range(1, 11):
+    # solicitar as notas do aluno
+    print(f"Informe as notas do aluno {i}:")
+    nota1 = float(input("Nota 1: "))
+    nota2 = float(input("Nota 2: "))
+    nota3 = float(input("Nota 3: "))
+    nota4 = float(input("Nota 4: "))
+    
+    # calcular a média do aluno e adicionar ao vetor de médias
+    media = (nota1 + nota2 + nota3 + nota4) / 4
+    medias.append(media)
+
+# contar o número de alunos com média maior ou igual a 7.0
+num_alunos_aprovados = 0
+for media in medias:
+    if media >= 7.0:
+        num_alunos_aprovados += 1
+
+# imprimir o número de alunos aprovados
+print(f"O número de alunos com média maior ou igual a 7.0 é: {num_alunos_aprovados}")
